1d0035a49f1d618c01802550f90a5aeff40ca5fb
Quoc Nam Luong Dai <NamLuong@Quocs-MacBook-Air.local>
2016-06-10 22:42:30 +0700

    init project

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name namLuongApp
+ * @description
+ * # namLuongApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('namLuongApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name namLuongApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the namLuongApp
+ */
+angular.module('namLuongApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
